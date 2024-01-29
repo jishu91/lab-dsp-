ba41ac541fbeaf5f71da2328b3e33956d5f381ef
jishu91 <jishukurmi@gmail.com>
2024-01-29 20:05:31 +0530

    29/1/24

---
+++ maxOf2num.py
@@ -0,0 +1,9 @@
+# user input
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+
+# maximum of value
+maximum_value = max(num1, num2)
+
+# print the value
+print("The maximum of two number:",maximum_value)
