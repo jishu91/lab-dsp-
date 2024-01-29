9a11ff0b3f479cf7169c8b03309112e901ae40f6
jishu91 <jishukurmi@gmail.com>
2024-01-29 19:26:42 +0530

    first commit

---
+++ add2num.py
@@ -0,0 +1,9 @@
+# user input
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+
+# Adding
+result = num1 + num2
+
+# print the added num
+print("The sum of two numbers is : ", result)
